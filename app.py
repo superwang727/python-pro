4d3a46abd2fcefbf9a7c62aa94aa16539b68dd0a
superwang727 <245801806@qq.com>
2021-06-05 16:25:47 +0800

    python second commit

--- app.py
+++ app.py
@@ -27,6 +27,17 @@ def hello_world1():
     }
     return jsonify(data)
 
+# /admin/edu/teacher/listPage/1/5
+@app.route('/admin/edu/teacher/listPage/1/5', methods=['GET'])
+@cross_origin(supports_credentials=True)
+def hello_world2():
+    data = {
+        'code': 20000,
+        'message': '成功',
+        'data': [{'name': 'admin', 'level': 1}]
+    }
+    return jsonify(data)
+
 
 if __name__ == '__main__':
     app.run()
